da09e1acea3693b1e6e97def7ed3f52689cd15e6
BrianSechelo <sechbrian123@gmail.com>
2024-08-08 22:04:05 +0000

    Task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+-- computes and store the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    where id = user_id;
+END;//
+DELIMITER ;
