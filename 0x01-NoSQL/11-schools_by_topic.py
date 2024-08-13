59a677136b467de3dbdb3805f604e69e6809868c
BrianSechelo <sechbrian123@gmail.com>
2024-08-13 06:05:34 -0700

    Task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,4 +2,4 @@
 """ Module docs """
 def schools_by_topic(mongo_collection, topic):
     """ Fucn Docs """
-    return list(mongo_collection.find( {"topic": topic} ))
+    return mongo_collection.find( {"topic": topic} )
